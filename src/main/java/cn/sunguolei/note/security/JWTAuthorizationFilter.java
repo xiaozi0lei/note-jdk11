7c6cbb349251f029d3a285d1afa14fe71bd5b500
GuoLei Sun <sunguolei@100tal.com>
2020-06-19 16:44:13 +0800

    try catch exception

--- src/main/java/cn/sunguolei/note/security/JWTAuthorizationFilter.java
+++ src/main/java/cn/sunguolei/note/security/JWTAuthorizationFilter.java
@@ -45,6 +45,9 @@ public class JWTAuthorizationFilter extends BasicAuthenticationFilter {
 
         // 通过验证 token 合法性获取授权
         UsernamePasswordAuthenticationToken authentication = getAuthentication(token.get());
+        if (authentication == null) {
+            response.sendRedirect("/toLogin");
+        }
 
         // 将获取到的用户对象设置为 SecurityContextHolder 中的用户对象，有了合法的 spring 用户对象，后面的过滤器就能取到 spring 用户
         SecurityContextHolder.getContext().setAuthentication(authentication);
@@ -56,17 +59,21 @@ public class JWTAuthorizationFilter extends BasicAuthenticationFilter {
 
         if (token != null) {
             // parse the token.
-            String username = Jwts.parser()
-                    .setSigningKey(JWT_KEY)
-                    .parseClaimsJws(token)
-                    .getBody()
-                    .getSubject();
+            try {
+                String username = Jwts.parser()
+                        .setSigningKey(JWT_KEY)
+                        .parseClaimsJws(token)
+                        .getBody()
+                        .getSubject();
 
-            // 解出 token，获取 token 中的 name，返回一个创建的合法 spring 用户，第三个参数 new ArrayList 是用来设置权限的
-            // 查出用户对象，设置到 authentication 对象中构建 Principal
-            if (username != null) {
-                User myUser = userService.findUserByUsername(username);
-                return new UsernamePasswordAuthenticationToken(myUser, null, new ArrayList<>());
+                // 解出 token，获取 token 中的 name，返回一个创建的合法 spring 用户，第三个参数 new ArrayList 是用来设置权限的
+                // 查出用户对象，设置到 authentication 对象中构建 Principal
+                if (username != null) {
+                    User myUser = userService.findUserByUsername(username);
+                    return new UsernamePasswordAuthenticationToken(myUser, null, new ArrayList<>());
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
             }
             return null;
         }
